6288f9a4b09aef90c53943f8ad36c216a4c2910e
tanweijun <a0125360@u.nus.edu>
2015-09-13 20:23:57 +0800

    Test sort. Edited code to ensure test is carried out properly. Test failed.

--- JUnitTest/TextBuddyTest.java
+++ JUnitTest/TextBuddyTest.java
@@ -16,22 +16,22 @@ public class TextBuddyTest {
 	    System.setOut(new PrintStream(printedContent));
 	}
 	
+	@Before
 	public void setUp() {
-		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add line for testing");
-		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add second line for testing");
-		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add third line for testing");
-		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add fourth line for testing");
-		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add fifth line for testing");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add a-line for testing");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add c-third line for testing");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add d-fourth line for testing");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add b-second line for testing");
 		//assertEquals("added to mytextfile.txt: \"line for testing\"\r\n", printedContent.toString());
 	}
 	
 	@Test
 	public void testSort() {
-		TextBuddy.executeCommandByType("mytextfile.txt", "display", "display");
-		String unsorted = printedContent.toString();
+		String expected = "1. a-line for testing\r\n2. b-second line for testing\r\n3. c-third line for testing\r\n4. d-fourth line for testing\r\n";
 		TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort");
-		String sorted = printedContent.toString();
-		assertEquals(unsorted, sorted);
+		TextBuddy.executeCommandByType("mytextfile.txt", "display", "display");
+		String actual = printedContent.toString();
+		assertEquals("check whether it is sorted", expected, actual);
 	}
 	
 	
