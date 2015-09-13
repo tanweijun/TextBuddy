1ea82f48b2d62d002c359cd33677a745ddc55c4b
tanweijun <a0125360@u.nus.edu>
2015-09-13 22:30:22 +0800

    Changed some of the sort test description. Added clear file to @Before so that file is always empty before a test. Test search. Failed.

--- JUnitTest/TextBuddyTest.java
+++ JUnitTest/TextBuddyTest.java
@@ -5,6 +5,12 @@ import org.junit.Test;
 
 public class TextBuddyTest {
 	
+	@Before
+	public void setUp() {
+		//clear the file to prevent the written contents to interfere with the testing
+		TextBuddy.executeCommandByType("mytextfile.txt", "clear", "clear");
+	}
+	
 	@Test
 	public void testSortCaps() {
 		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add A-line for testing");
@@ -14,10 +20,9 @@ public class TextBuddyTest {
 		
 		String expectedMsg = "File content has been sorted.";
 		String expectedContent = "1. A-line for testing\r\n2. B-fourth line for testing\r\n3. C-second line for testing\r\n4. D-third line for testing";
-		assertEquals("check whether it is sorted", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
+		assertEquals("check whether sort function ran successfully", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
 		assertEquals("check whether it is sorted", expectedContent, TextBuddy.executeCommandByType("mytextfile.txt", "display", "display"));
 		
-		TextBuddy.executeCommandByType("mytextfile.txt", "clear", "clear");
 	}
 	
 	@Test
@@ -29,10 +34,9 @@ public class TextBuddyTest {
 		
 		String expectedMsg = "File content has been sorted.";
 		String expectedContent = "1. a-line for testing\r\n2. d-aaathird line for testing\r\n3. d-second line for testing\r\n4. g-fourth line for testing";
-		assertEquals("check whether it is sorted", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
+		assertEquals("check whether sort function ran successfully", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
 		assertEquals("check whether it is sorted", expectedContent, TextBuddy.executeCommandByType("mytextfile.txt", "display", "display"));
 		
-		TextBuddy.executeCommandByType("mytextfile.txt", "clear", "clear");
 	}
 	
 	@Test
@@ -44,20 +48,36 @@ public class TextBuddyTest {
 		
 		String expectedMsg = "File content has been sorted.";
 		String expectedContent = "1. a-line for testing\r\n2. A-long fourth line for testing\r\n3. D-aaathird line for testing\r\n4. d-second line for testing";
-		assertEquals("check whether it is sorted", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
+		assertEquals("check whether sort function ran successfully", expectedMsg, TextBuddy.executeCommandByType("mytextfile.txt", "sort", "sort"));
 		assertEquals("check whether it is sorted", expectedContent, TextBuddy.executeCommandByType("mytextfile.txt", "display", "display"));
 	
-		TextBuddy.executeCommandByType("mytextfile.txt", "clear", "clear");
 	}
 	
 	@Test
 	public void testSearch() {
-		fail();
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add Once upon a time, there was a horse.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add The horse's name is MARY.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add Mary had a little lamb called HORSE.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add One fine day, a wolf named john came to visit.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add John ate both Mary and Horse.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add THE END.");
+
+		String expectedUpon = "1. Once upon a time, there was a horse.";
+		assertEquals("search same case", expectedUpon, TextBuddy.executeCommandByType("mytextfile.txt", "search", "search upon"));
+		
 	}
 	
 	@Test
 	public void testSearchWithNoResults() {
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add Once upon a time, there was a horse.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add The horse's name is MARY.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add Mary had a little lamb called HORSE.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add One fine day, a wolf named john came to visit.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add John ate both Mary and Horse.");
+		TextBuddy.executeCommandByType("mytextfile.txt", "add", "add THE END.");
+		
 		fail();
+		
 	}
 	
 }
