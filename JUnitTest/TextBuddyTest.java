b444cae258f74a149c1a022b29a0313bb3cdfcc4
tanweijun <a0125360@u.nus.edu>
2015-09-13 21:28:53 +0800

    Removed unneeded imports. Edited a method in Textbuddy.

--- JUnitTest/TextBuddyTest.java
+++ JUnitTest/TextBuddyTest.java
@@ -1,9 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
